5374a46d9396b116c327eeb854e5907c7718a2ca
tadakun8 <tadakun8.lab@gmail.com>
2021-11-17 10:47:50 +0900

    feat: Change app theme to black

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.black,
       ),
       home: const GalleryPage(),
     );
