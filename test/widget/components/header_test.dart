30bfb55fac4930fab06b8450ce5b4898ced9ca55
tadakun8 <tadakun8.lab@gmail.com>
2021-11-17 15:07:07 +0900

    refactor: Delete unneeded import

--- test/widget/components/header_test.dart
+++ test/widget/components/header_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gallery_clone/componensts/header.dart';
-import 'package:flutter_gallery_clone/shared/constants.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
